aa9735e110b0da624f742e6bd8a8db77ed677f70
polytetra1836 <polytetra183677@gmail.com>
2024-09-25 11:03:59 +0800

    Chicken1

--- Chicken1/Chicken.cpp
+++ Chicken1/Chicken.cpp
@@ -63,24 +63,24 @@ class Chicken
      
 };
 int main(){
-auto print = [](const Chicken &c){
-std::cout << "Hi, everyone! My name is " << c.getName()
-<< ", I am " << c.getAge() << " years old." << std::endl;
-};
-Chicken c(24, "Kunkun");
-print(c);
-Chicken d;
-d = c;
-print(d); // 测试【赋值运算符】是否正确，能正确输出给 20 分
-Chicken a = c;
-print(a); // 测试【赋值构造函数】是否正确，能正确输出给 20 分
-c.setName("Xukun Cai");
-print(c);
-print(a);
-print(d); // 测试是否为【深度复制】，本行与上两行能正确输出给 20 分
-Chicken b;
-b = d = c;
-print(b);
-print(d); // 测试【连续赋值】功能，本行与上一行能正确输出给 20 分
-return 0;
+    auto print = [](const Chicken &c){
+    std::cout << "Hi, everyone! My name is " << c.getName()
+    << ", I am " << c.getAge() << " years old." << std::endl;
+    };
+    Chicken c(24, "Kunkun");
+    print(c);
+    Chicken d;
+    d = c;
+    print(d);
+    Chicken a = c;
+    print(a); 
+    c.setName("Xukun Cai");
+    print(c);
+    print(a);
+    print(d); 
+    Chicken b;
+    b = d = c;
+    print(b);
+    print(d); 
+    return 0;
 }
