7ab2e4b401b6924b3c6d67501cdc4537374edbd6
polytetra1836 <polytetra183677@gmail.com>
2024-09-25 10:50:57 +0800

    Chiken1

--- Chicken1/Chicken.cpp
+++ Chicken1/Chicken.cpp
@@ -27,7 +27,11 @@ class Chicken
             age = _age;
         }
         void setName(const char *_name)
-        {
+        {   if (name != nullptr)
+            {
+                delete[] name;
+                name= nullptr;
+            }
             int len = strlen(_name) + 1;
             name = new char[len];
             for (int i = 0; i < len; i++)
@@ -44,28 +48,39 @@ class Chicken
             return age;
         }
 
-    Chicken& operator=(const Chicken& other){
-        if(this== &other) return *this;
+    Chicken& operator=(const Chicken& other)
+        { if(this== &other) return *this;
         delete[] name;
         age=other.age;
         if(other.name==nullptr){
             name=nullptr;
-            return *this;       }
-        int len = strlen(other.name) + 1;
-            name = new char[len];
-            for (int i = 0; i < len; i++)
-                name[i] = other.name[i];
+            return *this;       
+                                }
+        setName(other.name);
        
         return *this;
         }
      
 };
-int main()
-{
-    Chicken c(24, "Kunkun");
-    Chicken d;
-    d=c;
-    std::cout << "Hi, everyone! My name is " << c.getName() << ", I am " << c.getAge() << " years old." << std::endl;
-     std::cout << "Hi, everyone! My name is " << d.getName() << ", I am " << d.getAge() << " years old." << std::endl;
-    return 0;
+int main(){
+auto print = [](const Chicken &c){
+std::cout << "Hi, everyone! My name is " << c.getName()
+<< ", I am " << c.getAge() << " years old." << std::endl;
+};
+Chicken c(24, "Kunkun");
+print(c);
+Chicken d;
+d = c;
+print(d); // 测试【赋值运算符】是否正确，能正确输出给 20 分
+Chicken a = c;
+print(a); // 测试【赋值构造函数】是否正确，能正确输出给 20 分
+c.setName("Xukun Cai");
+print(c);
+print(a);
+print(d); // 测试是否为【深度复制】，本行与上两行能正确输出给 20 分
+Chicken b;
+b = d = c;
+print(b);
+print(d); // 测试【连续赋值】功能，本行与上一行能正确输出给 20 分
+return 0;
 }
